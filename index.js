73ac7d7f05a1d33bc726e5fb7195252b0e6bd514
aleksaar <aleksi.s.saarinen@gmail.com>
2022-04-26 14:59:04 +0300

    fix

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const { json } = require('express/lib/response')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) } )
 morgan.format('tiny2', ':method :url :status :res[content-length] - :response-time ms :body')
